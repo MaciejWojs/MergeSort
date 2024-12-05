8baf8ab32abb6b8da9ce425055b8325e64da752f
Maciej Wójs <misiek111403@gmail.com>
2024-12-05 16:02:24 +0100

    Doxygen comments main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,22 @@
+/**
+ * @file main.cpp
+ * @~english
+ * @brief A program that demonstrates the use of the merge sort algorithm.
+ * @~polish
+ * @brief Program demonstrujący użycie algorytmu sortowania przez scalanie.
+ */
 #include <iostream>
 #include <ctime>
 #include "MergeSort.hpp"
 
+ /**
+  * @~english
+  * @brief Main function that executes the merge sort demonstration.
+  * @return int Exit status of the program.
+  * @~polish
+  * @brief Główna funkcja wykonująca demonstrację sortowania przez scalanie.
+  * @return int Status zakończenia programu.
+  */
 int main() {
     srand(time(NULL));
     int size;
@@ -13,6 +28,12 @@ int main() {
 
     int* array = new int[size];
 
+    /**
+     * @~english
+     * @brief Fills the array with random values.
+     * @~polish
+     * @brief Wypełnia tablicę losowymi wartościami.
+     */
     for (int i = 0; i < size; i++) {
         array[i] = rand() % size;
     }
